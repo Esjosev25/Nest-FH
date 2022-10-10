c63065f2fb0299bf1571ef7b2dabd933cd2b00cf
Estuardo Villeda <estuardovilleda2001@gmail.com>
2022-10-09 18:05:41 -0600

    section 4 finished

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,20 +1,24 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
+
 
 @Injectable()
 export class CarsService {
-  private cars = [
+  private cars: Car[] = [
     {
-      id: 1,
+      id: uuid(),
       brand: 'Toyota',
       model: 'Corolla',
     },
     {
-      id: 2,
+      id: uuid(),
       brand: 'Honda',
       model: 'Civic',
     },
     {
-      id: 3,
+      id: uuid(),
       brand: 'Jeep',
       model: 'Cherokee',
     },
@@ -23,9 +27,44 @@ export class CarsService {
   public findAll() {
     return this.cars;
   }
-  public findOneById(id: number) {
+  public findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id ${id} Not Found`);
     return car;
+  }
+
+  public createCar(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+
+    this.cars.push(car);
+
+    return car;
+  }
+
+
+  public update( id: string, updateCarDto: UpdateCarDto){
+    let carDB = this.findOneById(id);
+    if( updateCarDto.id && updateCarDto.id !== id)
+      throw new BadRequestException('Car id inside body is not valid');
+    this.cars = this.cars.map( car => {
+      if (car.id == id) {
+        carDB = {
+          ...carDB,
+          ...updateCarDto,
+          id
+        }
+        return carDB;
+      }
+      return car;
+    })
+    return carDB;
+  }
+
+  public delete(id: string) {
+    const carDB = this.findOneById(id);
+    this.cars = this.cars.filter(car => car.id !== id)
   }
 }
