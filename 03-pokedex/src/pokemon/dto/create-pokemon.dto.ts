2bfc47883ceedc06e5dda7337186e69d8886073f
Estuardo Villeda <estuardovilleda2001@gmail.com>
2022-10-22 20:50:14 -0600

    finished section 7

--- 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsString, IsNotEmpty, IsInt, Min, IsPositive, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+  @IsInt()
+  @Min(1)
+  @IsPositive()
+  no: number;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  name: string;
+
+}
